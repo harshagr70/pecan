e8b87628871aa6c2c095f1c409d32ba7499ca65f
Yinghao Sun <sunyh@bu.edu>
2024-12-16 16:09:17 -0500

    Update runModule.run.write.configs.R
    Re-add the `write <- settings$database$bety$write` that was mistakenly deleted earlier.

--- base/workflow/R/runModule.run.write.configs.R
+++ base/workflow/R/runModule.run.write.configs.R
@@ -13,6 +13,7 @@ runModule.run.write.configs <- function(settings, overwrite = TRUE) {
     }
     return(PEcAn.settings::papply(settings, runModule.run.write.configs, overwrite = FALSE))
   } else if (PEcAn.settings::is.Settings(settings)) {
+    write <- settings$database$bety$write
     # double check making sure we have method for parameter sampling
     if (is.null(settings$ensemble$samplingspace$parameters$method)) settings$ensemble$samplingspace$parameters$method <- "uniform"
     ens.sample.method <-  settings$ensemble$samplingspace$parameters$method
